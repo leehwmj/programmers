b9c32e70168f95d550453924d57ce80a85c2c6a8
MooJu <leehwmj@core.today>
2017-11-19 14:27:09 +0900

    약수의 합 level 1 풀이완료

--- problem12.js
+++ problem12.js
@@ -7,8 +7,15 @@
 
 function sumDivisor(num) {
 	var answer = 0;
+	let sum = 0;
 
-	return answer;
+	for (let i = 1; i <= num; i++){
+		if(num%i === 0){
+			sum += i
+		}
+	}
+
+	return answer = sum;
 }
 
 // 아래는 테스트로 출력해 보기 위한 코드입니다.
