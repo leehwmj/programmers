951761b6e1e6ac4800044f75ba42b0b6c37ccd64
이현우 <leehwmj@gmail.com>
2019-06-08 14:36:03 +0900

    Create hash1.js

---
+++ hash1.js
@@ -0,0 +1,23 @@
+// 완주하지 못한 선수
+// dark light
+// sublime vim emacs
+// JavaScript 
+// 문제 설명
+
+// 수많은 마라톤 선수들이 마라톤에 참여하였습니다. 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
+// 마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
+// 제한사항
+// 마라톤 경기에 참여한 선수의 수는 1명 이상 100,000명 이하입니다.
+// completion의 길이는 participant의 길이보다 1 작습니다.
+// 참가자의 이름은 1개 이상 20개 이하의 알파벳 소문자로 이루어져 있습니다.
+// 참가자 중에는 동명이인이 있을 수 있습니다.
+
+function solution(participant, completion) {
+    for(var i = 0; i < completion.length; i++){
+        // var index = participant.findIndex(completion[i])
+        var element = completion[i]
+        var index = participant.indexOf(element)
+        participant.splice(index,1)
+    }
+    return( participant[0])
+}
